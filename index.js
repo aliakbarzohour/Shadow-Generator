5caa5ac803cdcbcc9a8af46310573c2e020b43bd
aliakbarzohour <aliakbar.zohour@gmail.com>
2022-03-19 13:09:33 +0330

    Coding notification and remove media directory

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Start Coding Shadow Generator ...
+
+// =========================================================
+// Code for Notification
+var toastTrigger = document.getElementById('liveToastBtn')
+var toastLiveExample = document.getElementById('liveToast')
+if (toastTrigger) {
+    toastTrigger.addEventListener('click', function() {
+        var toast = new bootstrap.Toast(toastLiveExample)
+
+        toast.show()
+    })
+}
+// =========================================================
